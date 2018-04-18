087dae5ccec092b37e5710b5d2611a24c2d8944e
Daniele Fornaciari <d.fornaciari@gmail.com>
2018-04-18 16:23:50 +0200

    moved to mysql2 + little refactor

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * use your custom table fields by setting: options.fields.
  * Example: options.fields = { level: 'mylevel', meta: 'metadata', message: 'source', timestamp: 'addDate'}
  * Two demo tables:
- * 
+ *
  CREATE TABLE `logtest`.`sys_logs_default` (
  `id` INT NOT NULL AUTO_INCREMENT,
  `level` VARCHAR(16) NOT NULL,
@@ -26,8 +26,9 @@
 
 
 var util = require('util'),
-  winston = require('winston'),
-  MySql = require('mysql');
+    winston = require('winston'),
+    MySql = require('mysql2'),
+    Transport = winston.Transport;
 
 /**
  * @constructor
@@ -36,55 +37,55 @@ var util = require('util'),
  * @param {String} options.database Database name
  * @param {String} options.table  Database table for the logs
  * @param {Object} **Optional** options.fields Log object, set custom fields for the log table
+ * @param {Object} **Optional** options.level Log level flag
  */
-var mysql = function(options) {
-  "use strict";
-  //Please visit https://github.com/felixge/node-mysql#connection-options to get default options for mysql module
-  this.options = options || {};
+var mysql = function (options) {
+    "use strict";
+    Transport.call(this, options);
 
-  if(!options.user){
-    throw new Error('The database username is required');
-  }
+    this.options = options || {};
 
-  if(!options.database){
-    throw new Error('The database name is required');
-  }
-
-  if(!options.table){
-    throw new Error('The database table is required');
-  }
-
-  //check custom table fields
-  if(!options.fields){
-
-    this.options.fields = {};
-    //use default names
-    this.fields = {
-      level : 'level',
-      meta: 'meta',
-      message : 'message',
-      timestamp: 'timestamp'
+    if (!options.user) {
+        throw new Error('The database username is required');
     }
 
-  }else{
-
-    //use custom table field names
-    this.fields = {
-      level : this.options.fields.level,
-      meta: this.options.fields.meta,
-      message : this.options.fields.message,
-      timestamp: this.options.fields.timestamp
+    if (!options.database) {
+        throw new Error('The database name is required');
     }
 
-  }
+    if (!options.table) {
+        throw new Error('The database table is required');
+    }
 
-  //Create a connection poll
-  this.pool = MySql.createPool(this.options);
+    //check custom table fields
+    if (!options.fields) {
+
+        this.options.fields = {};
+        //use default names
+        this.fields = {
+            level: 'level',
+            meta: 'meta',
+            message: 'message',
+            timestamp: 'timestamp'
+        }
+
+    } else {
+
+        //use custom table field names
+        this.fields = {
+            level: this.options.fields.level,
+            meta: this.options.fields.meta,
+            message: this.options.fields.message,
+            timestamp: this.options.fields.timestamp
+        }
+
+    }
+
+    //Create a connection poll
+    this.pool = MySql.createPool(this.options);
 
 };
 
-// Inherit from `winston.Transport`.
-util.inherits(mysql, winston.Transport);
 //logger name in winston
 mysql.prototype.name = 'mysql';
 //getter
@@ -97,42 +98,45 @@ winston.transports.Mysql = mysql;
  * @param meta  {Object} JSON object in winston
  * @param callback {function} callback when finished
  */
-mysql.prototype.log = function(level, message, meta, callback) {
-  "use strict";
+mysql.prototype.log = function (level, message, meta, callback) {
+    "use strict";
+    if (this.options.silent) {
+        return callback(null, true);
+    }
 
-  //save this
-  var self = this;
-  //run it in nextTick
-  process.nextTick(function() {
+    //save this
+    var self = this;
+    //run it in nextTick
+    process.nextTick(function () {
 
-    var pool = self.pool;
+        var pool = self.pool;
 
-    pool.getConnection(function (err, connection) {
+        pool.getConnection(function (err, connection) {
 
-      if(err){
-        return callback(err, null);
-      }
-      //connected
-      //set log object
-      var log = {};
-      log[self.fields.level] = level;
-      log[self.fields.message] = message;
-      log[self.fields.meta] = JSON.stringify(meta);
-      log[self.fields.timestamp] = new Date();
+            if (err) {
+                return callback(err, null);
+            }
+            //connected
+            //set log object
+            var log = {};
+            log[self.fields.level] = level;
+            log[self.fields.message] = message;
+            log[self.fields.meta] = JSON.stringify(meta);
+            log[self.fields.timestamp] = new Date();
 
-      //Save the log
-      connection.query('INSERT INTO '+ self.options.table + ' SET ?', log, function(err, rows, fields) {
-        if(err){
-          return callback(err, null);
-        }
-        //finished
-        connection.release();
-        callback(null, true);
-      });
+            //Save the log
+            connection.query('INSERT INTO ' + self.options.table + ' SET ?', log, function (err, rows, fields) {
+                if (err) {
+                    return callback(err, null);
+                }
+                //finished
+                connection.release();
+                callback(null, true);
+            });
+
+        });
 
     });
-
-  });
 
 };
 
